b467d565b85b354b798ce4e899cba0c17b6def92
Lindsey Dortch <codewithlinds@gmail.com>
2025-07-01 07:59:18 -0500

    Set up locations page frontend

---
+++ app/(routes)/locations/page.jsx
@@ -0,0 +1,23 @@
+import LocationsContext from '@/app/_components/locations/LocationsContext';
+import LocationsForm from '@/app/_components/locations/LocationsForm';
+import { getLocations } from '@/app/_lib/data-service';
+
+async function page() {
+  const locations = await getLocations();
+
+  console.log(locations);
+
+  return (
+    <section>
+      <h1>Locations</h1>
+      <div className="locations">
+        <LocationsContext locations={locations} />
+      </div>
+      <div className="addLocations">
+        <LocationsForm />
+      </div>
+    </section>
+  );
+}
+
+export default page;
