592da3209a1d819c4df7bb89b89a44931c962553
Lindsey Dortch <codewithlinds@gmail.com>
2025-07-20 14:32:49 -0500

    Styling for /villas page

--- app/(routes)/villas/page.jsx
+++ app/(routes)/villas/page.jsx
@@ -1,19 +1,20 @@
 import LinkButton from '@/app/_components/buttons/LinkButton';
 import Card from '@/app/_components/card/Card';
 import { getVillas } from '@/app/_lib/data-service';
+import styles from './page.module.scss';
 
 async function page() {
   const villas = await getVillas();
 
   return (
     <section>
-      <div className="header">
+      <div className={styles.header}>
         <h1>Villas</h1>
         <LinkButton link="/villas/add" color="pink">
           Add New Villa
         </LinkButton>
       </div>
-      <div className="villas">
+      <div className={styles.villas}>
         {villas.map((v) => (
           <Card villa={v} key={v.id} />
         ))}
--- app/_components/card/Card.jsx
+++ app/_components/card/Card.jsx
@@ -1,10 +1,11 @@
 'use client';
 
 import LinkButton from '@/app/_components/buttons/LinkButton';
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDelete } from 'react-icons/md';
 import styles from '@/app/_components/card/card.module.scss';
 import { deleteVilla } from '@/app/_lib/data-service';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 function Card({ villa }) {
   const router = useRouter();
@@ -15,18 +16,40 @@ function Card({ villa }) {
   };
 
   return (
-    <div>
-      <p>{villa.name}</p>
-      <p>{villa.location.location}</p>
-      <LinkButton link={`/villas/${villa.id}`} color="purple">
-        View
-      </LinkButton>
-      <LinkButton link={`/villas/edit/${villa.id}`} color="purple">
-        Edit
-      </LinkButton>
-      <button onClick={() => handleDelete(villa.id)}>
-        <MdDeleteForever />
-      </button>
+    <div className={styles.card}>
+      <div className={styles.cardImage}>
+        <Image
+          src={villa.main_image}
+          alt="Villa Image"
+          fill
+          priority
+          sizes="auto"
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+          }}
+        />
+      </div>
+      <div className={styles.cardInfo}>
+        <div className="headers">
+          <h3>{villa.name}</h3>
+          <h4>{villa.location.location}</h4>
+        </div>
+        <div className={styles.buttons}>
+          <LinkButton link={`/villas/${villa.id}`} color="pink">
+            View
+          </LinkButton>
+          <LinkButton link={`/villas/edit/${villa.id}`} color="purple">
+            Edit
+          </LinkButton>
+          <button
+            onClick={() => handleDelete(villa.id)}
+            className={styles.deleteBtn}
+          >
+            <MdDelete />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
